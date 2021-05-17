616c4aaa015e17c015129290f5d832d6d33132cb
root <root@a0d7b954-ssh.local.hass.io>
2021-05-17 15:05:45 -0400

    Add v3 support

--- custom_components/assistant_relay_tts/tts.py
+++ custom_components/assistant_relay_tts/tts.py
@@ -1,26 +1,27 @@
 """Support for the Google Assistant TTS speech service."""
 import logging
 import requests
+import ffmpeg
 import voluptuous as vol
 from homeassistant.components.tts import CONF_LANG, PLATFORM_SCHEMA, Provider
 import homeassistant.helpers.config_validation as cv
 
 _LOGGER = logging.getLogger(__name__)
 
-CONF_PITCH = "pitch"
-CONF_BITRATE = "bitrate"
+CONF_IP = "ip"
+CONF_PORT = "port"
 
 DEFAULT_LANG = "Google-US-English"
-DEFAULT_PITCH = "100"
-DEFAULT_BITRATE = "128k"
+DEFAULT_IP = '127.0.0.1'
+DEFAULT_PORT =  '3000'
 
 SUPPORTED_LANGUAGES = ['Google-US-English']
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
     {
         vol.Optional(CONF_LANG, default=DEFAULT_LANG): vol.In(SUPPORTED_LANGUAGES),
-        vol.Optional(CONF_PITCH, default=DEFAULT_PITCH): cv.string,
-        vol.Optional(CONF_BITRATE, default=DEFAULT_BITRATE): cv.string,
+        vol.Optional(CONF_IP, default=DEFAULT_IP): cv.string,
+        vol.Optional(CONF_PORT, default=DEFAULT_PORT): cv.string,
     }
 )
 
@@ -29,19 +30,19 @@ def get_engine(hass, config, discovery_info=None):
     """Set up Google speech component."""
     return GoogleProvider(
         config[CONF_LANG],
-        config[CONF_PITCH],
-        config[CONF_BITRATE],
+        config[CONF_IP],
+        config[CONF_PORT],
     )
 
 
 class GoogleProvider(Provider):
     """The Google TTS API provider."""
 
-    def __init__(self, lang, pitch, bitrate):
+    def __init__(self, lang, ip, port):
         """Initialize Google TTS provider."""
         self._lang = lang
-        self._pitch = pitch
-        self._bitrate = bitrate
+        self._ip = ip
+        self._port = port
         self.name = "GoogleTTS"
 
     @property
@@ -59,8 +60,15 @@ class GoogleProvider(Provider):
         if language is None:
             language = self._lang
         try:
-            r = requests.get('http://192.168.1.105:4000/tts?text=' + message)
+            r = requests.post('http://' + self._ip + ':' + self._port + '/assistant',  data = {'command':'repeat after me ' + message})
+            audio_url = ('http://' + self._ip + ':' + self._port + r.json()["audio"])
+            out, _ = (ffmpeg
+                .input(audio_url)
+                .output('-', format='mp3', ss=0.566)
+                .overwrite_output()
+                .run(capture_stdout=True)
+            )
         except Exception as e:
             _LOGGER.error("Error while to convert: %s", str(e))
             return (None, None)
-        return ("mp3", r.content)
+        return ("mp3", out)
